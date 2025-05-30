47dddd5f47b7e23d49f9475ae6f0f59e9aed3dc3
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-05-30 17:23:26 -0300

    adicionado classe Departament

---
+++ DAO-Project/src/model/entities/Departament.java
@@ -0,0 +1,55 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departament implements Serializable {
+    private int id;
+    private String name;
+
+    public Departament() {
+    }
+
+    public Departament(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Departament that = (Departament) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Departament{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
