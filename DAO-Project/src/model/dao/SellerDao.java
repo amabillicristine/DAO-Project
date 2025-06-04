47d90c244162c068494bddbcea6fa4dd0e6ca34a
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-06-04 17:38:43 -0300

    Criação de Dao factory
    Signed-off-by: Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>

--- DAO-Project/src/model/dao/SellerDao.java
+++ DAO-Project/src/model/dao/SellerDao.java
@@ -7,11 +7,18 @@ import java.util.List;
 
 public interface SellerDao {
     void insert(Seller obj);
+
     void update(Seller obj);
+
     void deleteById(Integer id);
+
     Seller findById(Integer id);
+
     List<Seller> findall();
+
     List<Seller> findByDepartment(Department department);
+
+    List<Seller> findAll();
 
 }
 
